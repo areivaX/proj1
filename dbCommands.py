6379a7963431fc7904ced2c7c974e32612bb2396
areivax <xavierajustine@gmail.com>
2020-04-28 18:45:57 -0400

    csv to sql done

---
+++ dbCommands.py
@@ -0,0 +1,39 @@
+import os
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
+
+engine = create_engine(os.getenv("DATABASE_URL"))
+db = scoped_session(sessionmaker(bind=engine))
+
+
+def a1():
+    db.execute("CREATE TABLE authors (author_id SERIAL PRIMARY KEY, name VARCHAR NOT NULL) ")
+    db.commit()
+
+def a10():
+    db.execute("ALTER TABLE authors RENAME COLUMN author_id TO id")
+    db.commit()
+
+
+def a2():
+    db.execute("CREATE TABLE books " \
+                    "(isbn INTEGER PRIMARY KEY, "\
+                    "title VARCHAR, "\
+                    "author_id INTEGER REFERENCES authors )")
+    db.commit()
+
+
+def a3():
+    db.execute("ALTER TABLE authors ADD CONSTRAINT a_name unique(name)")
+    db.commit()
+
+
+
+def a4():
+    db.execute("ALTER TABLE books ADD year INTEGER")
+    db.commit()
+
+def a5():
+    db.execute("ALTER TABLE books ALTER COLUMN isbn TYPE VARCHAR")
+    db.commit()
---
+++ import.py
@@ -0,0 +1,48 @@
+import csv
+import os
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
+
+engine = create_engine(os.getenv("DATABASE_URL"))
+db = scoped_session(sessionmaker(bind=engine))
+
+
+def a1():
+    f = open("books.csv")
+    reader = csv.reader(f)
+    fields = next(reader)
+    count = 0
+    for line in reader:
+        count +=1
+        auth_name = line[2]
+        if db.execute("SELECT * FROM authors WHERE name = :name", {"name": auth_name}).fetchone() is None:
+            db.execute('INSERT INTO authors (name) VALUES (:author)', {"author":auth_name})
+            print(f"inserting on count {count}")
+    db.commit()
+
+
+
+
+def a2():
+    f = open("books.csv")
+    #suppose there are no book duplicates
+    reader = csv.reader(f)
+    fields = next(reader)
+    for line in reader:
+        isbn,title,author,year = line
+        print(f"looking for {author}")
+        x = db.execute("SELECT * FROM authors WHERE name = :author", {"author":author}).first()
+        author_id = x.id
+
+        db.execute("INSERT INTO books (isbn, title, year, author_id) VALUES " \
+                    "(:isbn, :title, :year, :author_id)",
+                    {"isbn":isbn, "title":title, "year":year, "author_id":author_id})
+    db.commit()
+
+def main():
+    a1()
+    a2()
+
+if __name__ == "__main__":
+    main()
